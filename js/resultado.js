364acc6c46ae78c10a6c0077070830789cf3b07c
Ashending <faby.hc2051@outlook.com>
2024-06-17 20:30:47 -0400

    Added crear.html for table Resultado

--- js/resultado.js
+++ js/resultado.js
@@ -34,6 +34,40 @@ function completarFila(element,index,arr){
 }
 
 
+function agregarResultado(){
+  //Obtenemos el tipo de gestión que ingresa el usuario
+  let nombre_resultado = document.getElementById("txt_nombre").value;
+
+  //Encabezado de la solicitud
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  let fechaHoraActual = obtenerFechaHora();
+  //Carga util de datos
+  const raw = JSON.stringify({
+    "nombre_resultado": nombre_resultado,
+    "fecha_registro": fechaHoraActual
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+  };
+
+  // Request tabla resultado
+  fetch("http://144.126.210.74:8080/api/resultado", requestOptions)
+    .then((response) => {
+      if(response.status == 200) {
+        location.href ="listar.html";
+      }
+    })
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
+}
+
+
 function formatearFechaHora(fecha_registro){
   let fechaHoraActual = new Date(fecha_registro);
   let fechaHoraFormateada = fechaHoraActual.toLocaleString('es-ES',{
@@ -46,5 +80,22 @@ function formatearFechaHora(fecha_registro){
     second : '2-digit',
     timeZone:'UTC'
   }).replace(/(\d+)\/(\d+)\/(\d+)\,\s*(\d+):(\d+):(\d+)/,'$3-$2-$1 $4:$5:$6');
+  return fechaHoraFormateada;
+}
+
+
+function obtenerFechaHora() {
+  let fechaHoraActual = new Date();
+
+  let fechaHoraFormateada = fechaHoraActual.toLocaleString('es-ES',{
+    hour12 :false,
+    year :'numeric',
+    month :'2-digit',
+    day:'2-digit',
+    hour : '2-digit',
+    minute :'2-digit',
+    second : '2-digit'
+  }).replace(/(\d+)\/(\d+)\/(\d+)\,\s*(\d+):(\d+):(\d+)/,'$3-$2-$1 $4:$5:$6');
+
   return fechaHoraFormateada;
 }
